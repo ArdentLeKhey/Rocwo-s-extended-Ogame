1f3c36597212482190271a553c195aeca9de9bfc
Ardent <61157993+ArdentLeKhey@users.noreply.github.com>
2021-03-15 14:29:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 // @namespace    https://github.com/ArdentLeKhey
 // @version      0.1
 // @description  Displays the exact bidding time (note that this time is randomized and that this script only displays a time as an indication)
-// @author       Rocwo
+// @author       ArdentLeKhey
 // @grant        none
 // @include      *.ogame*gameforge.com/game/index.php*
+// @include      about:addons
 // ==/UserScript==
 
 
